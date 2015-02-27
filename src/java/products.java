79da688baba452046beee741648206dbe18f8757
SwarShah <swar12894@gmail.com>
2015-02-27 12:02:53 -0500

    Implemented doGet with initial JSON output.

---
+++ src/java/products.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import credentials.Credentials;
+import org.json.simple.*;
+/**
+ *
+ * @author Swar
+ */
+@WebServlet(urlPatterns = {"/products"})
+public class products extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private String getResults(String query, String... params) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        JSONObject json = new JSONObject();
+        try (Connection cn = Credentials.getConnection()) {
+            PreparedStatement pstmt = cn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            
+            while (rs.next()) {
+                JSONObject tmpjson = new JSONObject();
+                if(!sb.toString().equals("[ ")){
+                    sb.append(",\n");
+                }
+                json.put("quantity", rs.getInt("quantity"));
+                json.put("productId", rs.getInt("productId"));
+                json.put("name", rs.getString("name"));
+                json.put("description", rs.getString("description"));
+                
+                sb.append(json.toJSONString());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //return sb.toString();
+        sb.append(" ]");
+        return sb.toString();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setHeader("Content-Type", "text/plain-text");
+        try (PrintWriter out = response.getWriter()) {
+            if (!request.getParameterNames().hasMoreElements()) {
+                out.println(getResults("SELECT * FROM PRODUCT"));
+            } else {
+                int id = Integer.parseInt(request.getParameter("id"));
+                out.println(getResults("SELECT * FROM PRODUCT WHERE productId = ?", String.valueOf(id)));
+                
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
